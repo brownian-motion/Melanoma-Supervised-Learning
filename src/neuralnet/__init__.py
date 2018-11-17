c6a44874cd501ba4a2d94f96ed6a1b6033e621a5
JJ Brown <jack.brown.255@gmail.com>
2018-11-17 16:16:17 -0600

    Added documentation, and refactored out common base class for re-use when implementing CNNs
    Signed-off-by: JJ Brown <jack.brown.255@gmail.com>

--- src/neuralnet/__init__.py
+++ src/neuralnet/__init__.py
@@ -24,34 +24,19 @@ class SoftmaxLayer:
         return exps / sum(exps)
 
 
-class AbstractPoolingLayer:
-    def __init__(self, tile_shape, func=numpy.amax, overlap_tiles=False):
+class AbstractConvolutionalLayer:
+    '''
+    Implements basic convolution for a neural net layer,
+    which breaks up an input matrix into tiles of a particular shape
+    so that each tile can be processed separately.
+    '''
+
+    def __init__(self, tile_shape, overlap_tiles=False):
         if len(tile_shape) != 2:
             raise ValueError(
                 "This pooling layer implementation only knows how to handle 2-D tiles, inputs, and outputs")
         self.tile_shape = tile_shape
-        self.func = func
         self._overlap_tiles = overlap_tiles
-
-    def _compute_output_shape(self, input_shape, tile_shape):
-        if self._overlap_tiles:
-            return self._compute_output_shape_with_overlap(input_shape, tile_shape)
-        return self._compute_output_shape_non_overlap(input_shape, tile_shape)
-
-    def _compute_output_shape_non_overlap(self, input_shape, tile_shape):
-        return tuple(math.ceil(input_shape[i] / tile_shape[i]) for i in range(len(tile_shape)))
-
-    def _compute_output_shape_with_overlap(self, input_shape, tile_shape):
-        return tuple(math.ceil(input_shape[i] - tile_shape[i] + 1) for i in range(len(tile_shape)))
-
-    def process(self, input):
-        output_shape = self._compute_output_shape(input.shape, self.tile_shape)
-        output = numpy.zeros(output_shape)
-        for i in range(output_shape[0]):
-            for j in range(output_shape[1]):
-                tile = self._get_tile((i, j), input, self.tile_shape)
-                output[i, j] = self.func(tile)
-        return output
 
     def _get_tile(self, tile_coord, input, tile_shape):
         if len(tile_shape) != 2 or len(tile_coord) != 2:
@@ -75,7 +60,48 @@ class AbstractPoolingLayer:
         assert tile.shape == tile_shape
         return tile
 
+    def _compute_output_shape(self, input_shape, tile_shape):
+        if self._overlap_tiles:
+            return self._compute_output_shape_with_overlap(input_shape, tile_shape)
+        return self._compute_output_shape_non_overlap(input_shape, tile_shape)
+
+    @staticmethod
+    def _compute_output_shape_non_overlap(input_shape, tile_shape):
+        return tuple(math.ceil(input_shape[i] / tile_shape[i]) for i in range(len(tile_shape)))
+
+    @staticmethod
+    def _compute_output_shape_with_overlap(input_shape, tile_shape):
+        return tuple(math.ceil(input_shape[i] - tile_shape[i] + 1) for i in range(len(tile_shape)))
+
+
+class AbstractPoolingLayer(AbstractConvolutionalLayer):
+    '''
+    Implements a pooling layer for a neural net,
+    which divides an input matrix/vector into 'tiles'
+    and then performs a particular function on each tile
+    to compute the layer's output.
+    '''
+
+    def __init__(self, tile_shape, func=numpy.amax, overlap_tiles=False):
+        super().__init__(tile_shape, overlap_tiles)
+        self.func = func
+
+    def process(self, input):
+        output_shape = self._compute_output_shape(input.shape, self.tile_shape)
+        output = numpy.zeros(output_shape)
+        for i in range(output_shape[0]):
+            for j in range(output_shape[1]):
+                tile = self._get_tile((i, j), input, self.tile_shape)
+                output[i, j] = self.func(tile)
+        return output
+
 
 class MaxpoolLayer(AbstractPoolingLayer):
+    '''
+    Performs max pooling on a matrix of input values
+    as a layer in a neural net.
+    Max pooling divides up the input matrix into 'tiles' or 'pools'
+    and returns the maximum value for each pool as its output.
+    '''
     def __init__(self, tile_shape, overlap_tiles=False):
         super().__init__(tile_shape, func=numpy.amax, overlap_tiles=overlap_tiles)
