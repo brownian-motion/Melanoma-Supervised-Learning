cc8d500bd220eb2ff850a97edad904b1c20b8468
JJ Brown <jack.brown.255@gmail.com>
2018-12-02 12:26:09 -0600

    Normalized image dimensions. Now we can approach feature extraction in a uniform way for ALL samples.
    Signed-off-by: JJ Brown <jack.brown.255@gmail.com>

--- src/images/__init__.py
+++ src/images/__init__.py
@@ -7,6 +7,8 @@ import cv2
 
 from images.cropping import crop_to_centered_square
 
+STANDARD_IMAGE_LENGTH = 768
+
 
 class Sample:
     def __init__(self, name, diagnosis, image_dim, filename):
@@ -21,14 +23,14 @@ class Sample:
         For example, if this image is 1000x760, then it returns a 760x760x3 matrix.
         :return: a square NumPy matrix of the RGB triplets in the central square of this Sample's image.
         '''
-        return crop_to_centered_square(self.get_image_raw())
+        return cv2.resize(crop_to_centered_square(self.get_image_raw()), (STANDARD_IMAGE_LENGTH, STANDARD_IMAGE_LENGTH))
 
     def get_image_raw(self):
         return cv2.imread(self.filename)
 
     def get_grayscale_image(self):
         '''
-        Returns a NumPy matrix of grayscale valeus for each pixel
+        Returns a NumPy matrix of grayscale values for each pixel
         :return:
         '''
         return cv2.cvtColor(self.get_image(), cv2.COLOR_BGR2GRAY)
--- src/neuralnet/experiment.py
+++ src/neuralnet/experiment.py
@@ -1,17 +1,15 @@
 import random
 
 import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
 
 from cross_entropy import binary_cross_entropy
 from images import *
 from neuralnet import *
 
-APPROVED_SIZE = (1504, 1129)
-
 
 def main():
     samples = Sample.get_samples("../../ISIC-images/UDA-1")
-    samples = [sample for sample in samples if sample.image_dim == APPROVED_SIZE]
     # for sample in samples:
     #     print("(%4d, %4d) - %s" % (sample.image_dim[0], sample.image_dim[1], sample.diagnosis))
     #
@@ -19,8 +17,9 @@ def main():
     # net.add_layer(MinpoolLayer((2, 2), overlap_tiles=False))  # take out border
     # net.add_layer(MeanpoolLayer((2, 2), overlap_tiles=False))  # make the image smaller
     net.add_layer(ConvolutionalLayer((3, 3), 0.01))
-    net.add_layer(FullyConnectedLayer((1129 - 3 + 1) * (1129 - 3 + 1), 4, 0.001, 'sigmoid'))
-    net.add_layer(FullyConnectedLayer(4, 2, 0.01, 'relu'))
+    net.add_layer(
+        FullyConnectedLayer((STANDARD_IMAGE_LENGTH - 3 + 1) * (STANDARD_IMAGE_LENGTH - 3 + 1), 12, 0.001, 'sigmoid'))
+    net.add_layer(FullyConnectedLayer(12, 2, 0.01, 'relu'))
     net.add_layer(SoftmaxLayer())
 
     random.shuffle(samples)
@@ -48,6 +47,7 @@ def main():
         print(entropy)
 
     plt.plot(pos_sample_nums, pos_entropies, 'r-', neg_sample_nums, neg_entropies, 'b-')
+    plt.legend(handles=[Patch(color='red', label='Melanoma'), Patch(color='blue', label='Not Melanoma')])
     plt.show()
 
 
