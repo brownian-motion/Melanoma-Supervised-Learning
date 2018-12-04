031888fcf7e772b93d6126a6d87dacb6a7c7f8d6
Ryan Gately <ryanegately@icloud.com>
2018-12-03 21:18:31 -0600

    started feature extraction stuff

--- src/naive_bayes/naive_bayes.py
+++ src/naive_bayes/naive_bayes.py
@@ -72,6 +72,7 @@ def probabilityDensity(x, mean, stdev):
     exponent = math.exp(-(math.pow(x-mean,2) / (2*math.pow(stdev,2))))
     return exponent * (1 / (math.sqrt(2*math.pi) * stdev))
 
+# gaussian distribution fix
 def gaussianProbability(x, mean, variance):
     exponent = math.exp(-(math.pow(x-mean,2) / (2*variance)))
     return exponent * (1 / (math.sqrt(2*math.pi * variance)))
