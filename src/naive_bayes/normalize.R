f2b699a63f245745888cd48739a2081fef8c7fc1
ryangately <ryangately@protonmail.com>
2018-12-13 21:16:41 -0600

    backup of additional test data

--- src/naive_bayes/normalize.R
+++ src/naive_bayes/normalize.R
@@ -6,3 +6,11 @@ norm_data$V129<-NULL
 norm_data = as.data.frame(apply(norm_data, 2, normalize))
 norm_data$V129 = orig_data$V129
 write.csv(norm_data, "melanoma_normalized_data.csv", row.names=FALSE)
+
+prob_data = read.csv("probabilities.csv", sep=",", header=FALSE)
+
+normalize = function(xs){(xs - mean(xs))/sd(xs)}
+norm_data = prob_data
+norm_data = as.data.frame(apply(norm_data, 2, normalize))
+norm_data$V1 = prob_data$V1
+write.csv(norm_data, "prob_normalized_data.csv", row.names=FALSE)
